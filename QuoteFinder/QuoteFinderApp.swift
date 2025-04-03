393dbdebf44e5b2a8796b3615f669e7756024af4
Hannu Packalen <hpackalen@lcs.on.ca>
2025-04-03 08:46:11 -0400

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Hannu Packalen on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
