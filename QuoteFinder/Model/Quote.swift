e3459f91c7cf1629a41e45bc7ce2c5a86f0d131e
Hannu Packalen <hpackalen@lcs.on.ca>
2025-04-04 12:00:16 -0400

    Added a computed property to the model to hold the quote and author together.

--- QuoteFinder/Model/Quote.swift
+++ QuoteFinder/Model/Quote.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 struct Quote: Identifiable, Codable {
+    
+    //MARK: Stored Properties
     let id = UUID()
     let quoteAuthor: String?
     let quoteText: String?
+    
+    //MARK: Computed Properties
+    var AuthorAndQuote: String {
+        
+        if let author = self.quoteAuthor, let quote = self.quoteText {
+            return "\(quote)\n\n\(author)"
+        } else {
+            return ""
+        }
+        
+    }
 }
